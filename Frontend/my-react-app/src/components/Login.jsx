6097b291b3e1d18a912c644007a414e2d92f424d
Adrian <adrianserban928@yahoo.com>
2025-05-23 12:36:05 +0300

    first try for frontend login and register

---
+++ Frontend/my-react-app/src/components/Login.jsx
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () =>{
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const response = await axios.post('http://localhost:3000/login',{
+                userName,
+                password,
+            },{
+                headers: {'Content-Type':'application/json'},
+            }
+        );
+        localStorage.setItem('token',response.data.token);
+        navigate('/Dashboard')
+        }catch(err){
+            const errorMessage = err.response?.data?.error;
+            console.log(errorMessage);
+        }
+    }
+
+    return(
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    placeholder="UserName"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button>Login</button>
+            </form>
+        </div>
+    );
+}
+export default Login
---
+++ Frontend/my-react-app/src/components/Register.jsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import React, {useState} from "react";
+import { useNavigate } from 'react-router-dom';
+
+const Register = () =>{
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        try{
+            const response = await axios.post('http://localhost:3000/register', {
+                userName,
+                password,
+                confirmPassword,
+            },{
+                headers: {'Content-Type':'application/json'}
+            });
+            localStorage.setItem('token', response.data.token);
+            navigate('/');
+        }catch(err){
+            const errorMessage = err.response?.data?.error;
+            console.log(errorMessage);
+        }
+    }
+
+    return(
+        <div>
+            <h2>Register</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                placeholder="Username"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                />
+                <input 
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} 
+                />
+                <input 
+                type="password" 
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+            </form>
+        </div>
+    );
+
+}
+export default Register
